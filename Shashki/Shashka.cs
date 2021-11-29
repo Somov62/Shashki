77d9f84b69337b72e00269c38eea7cf311ddc872
Somov <misha2003980@gmail.com>
2021-11-29 21:55:09 +0300

    Начинаем заниматься дамками (*страшные звуки из глубин проекта*) Сделал текстуру для дамки

--- Shashki/Shashka.cs
+++ Shashki/Shashka.cs
@@ -13,11 +13,14 @@ namespace Shashki
     {
         Border _figure;
         private bool _isSelected;
+        private bool _isDamka;
         #region Colors
         private static readonly Color blackBorder = new() { A = 255, R = 40,  G = 40,  B = 40  };
         private static readonly Color blackBack   = new() { A = 255, R = 160, G = 160, B = 160 };
+        private static readonly Color blackDamka  = new() { A = 255, R = 80,  G = 80,  B = 80 };
         private static readonly Color whiteBorder = new() { A = 255, R = 147, G = 147, B = 147 };
         private static readonly Color whiteBack   = new() { A = 255, R = 255, G = 255, B = 255 };
+        private static readonly Color whiteDamka  = new() { A = 255, R = 200, G = 200, B = 200 };
         private readonly RadialGradientBrush _black = new()
         {
             GradientStops = new GradientStopCollection()
@@ -46,6 +49,34 @@ namespace Shashki
                 new GradientStop(whiteBack, 0.915),
             }
         };
+        private readonly RadialGradientBrush _blackDamka = new()
+        {
+            GradientStops = new GradientStopCollection()
+            {
+                //new GradientStop(blackBorder, 0.317),
+                new GradientStop(blackDamka, 0.43),
+                new GradientStop(blackDamka, 0.59),
+                new GradientStop(blackBack, 0.78),
+                new GradientStop(blackDamka, 0.234),
+                new GradientStop(blackBorder, 0.689),
+                new GradientStop(blackBack, 0.915),
+                new GradientStop(blackBorder, 0.983),
+            }
+        };
+        private readonly RadialGradientBrush _whiteDamka = new()
+        {
+            GradientStops = new GradientStopCollection()
+            {
+                //new GradientStop(whiteBorder, 0.317),
+                new GradientStop(whiteDamka, 0.43),
+                new GradientStop(whiteDamka, 0.59),
+                new GradientStop(whiteBack, 0.78),
+                new GradientStop(whiteDamka, 0.234),
+                new GradientStop(whiteBorder, 0.689),
+                new GradientStop(whiteBack, 0.915),
+                new GradientStop(whiteBorder, 0.983),
+            }
+        };
         #endregion
         public Shashka(int rowCoord, int columnCoord, Team team)
         {
@@ -67,7 +98,14 @@ namespace Shashki
                 else _figure.BorderBrush = Brushes.Transparent;
             }
         }
-        public bool IsDamka { get; set; }
+        public bool IsDamka { get => _isDamka; set { 
+                _isDamka = value;
+                if (value && Color == Team.White) _figure.Background = _whiteDamka;
+                if (value && Color == Team.Black) _figure.Background = _blackDamka;
+                if (!value && Color == Team.White) _figure.Background = _white;
+                if (!value && Color == Team.Black) _figure.Background = _black;
+            } 
+        }
         public Border Figure { get => _figure; }
     }
     public enum Team
